b31c9f66a5af20dcb244cdbba5b5747df243c7cd
atanas <charleatanasi@gmail.com>
2021-08-19 20:38:30 +0300

    Implement weather data model for new API

--- app/src/main/java/com/tana/weatherapp/Constants.kt
+++ app/src/main/java/com/tana/weatherapp/Constants.kt
@@ -1,6 +1,10 @@
 package com.tana.weatherapp
 
 const val TAG = "MAinActivity"
+
+const val BASE_URL = "https://api.openweathermap.org/"
+const val API_KEY = "a3e284e80d8b35ce1f389e45a5cf6ed9"
+
 const val DEFAULT_UPDATE_INTERVAL: Long = 30000
 const val FAST_UPDATE_INTERVAL: Long = 5000
 const val LOCATION_REQUEST_CODE = 99
